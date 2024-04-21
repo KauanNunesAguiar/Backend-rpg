07961a2a8087aa159c242cf74c3b18358ca02a79
Kauan Nunes Aguiar <133172749+KauanNunesAguiar@users.noreply.github.com>
2024-04-21 17:14:39 -0300

    Integração com API

--- charactercreation/src/main/java/com/rpgded/charactercreation/Controllers/PersonagemController.java
+++ charactercreation/src/main/java/com/rpgded/charactercreation/Controllers/PersonagemController.java
@@ -2,7 +2,9 @@ package com.rpgded.charactercreation.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 import com.rpgded.charactercreation.Models.Personagem;
 import com.rpgded.charactercreation.Models.PersonagemRequest;
@@ -12,9 +14,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RestController
 @RequestMapping("/personagem")
 public class PersonagemController {
+
+    private RestTemplate restTemplate = new RestTemplate();
+    private final String ROLL_API_URL = "https://rpg-dice-roller-api.djpeacher.com/api/roll/";
 
     @Autowired
     private PersonagemService personagemService;
@@ -50,6 +58,33 @@ public class PersonagemController {
         return null;
     }
 
+    @GetMapping("/rolar/{notacao}")
+    public ResponseEntity<String> rolagem(@PathVariable String notacao) {
+        int rolledValue = rolarDado(notacao);
+        return ResponseEntity.ok("Valor rolado: "+ notacao + " = " + rolledValue);
+    }
+
+    public int rolarDado(String notacao) {
+        // Fazendo solicitação à API de rolagem de dados
+        String apiUrl = ROLL_API_URL + notacao;
+        ResponseEntity<String> resposta = restTemplate.getForEntity(apiUrl, String.class);
+
+        // Processando resposta da API
+        if (resposta.getStatusCode() == HttpStatus.OK) {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode jsonNode = mapper.readTree(resposta.getBody());
+
+                int total = jsonNode.get("total").asInt();
+                return total;
+            } catch (Exception e) {
+                throw new RuntimeException("Erro ao processar resposta da API de rolagem de dados", e);
+            }
+        } else {
+            throw new RuntimeException("Falha ao rolar o dado.");
+        }
+    }
+
     @GetMapping("/procurar/nome:{nome}")
     public Personagem getPersonagem(@PathVariable String nome) {
         for (Personagem personagem : listaDePersonagens) {
@@ -60,16 +95,30 @@ public class PersonagemController {
         return null;
     }
 
-    @GetMapping("/açao/id:{id}/{acao}/{valor}")
+    @GetMapping("/açao/id:{id}/{acao}/valor:{valor}")
     public String acao(@PathVariable Long id, @PathVariable String acao, @PathVariable Long valor) {
         Personagem personagem = getPersonagem(id);
         return personagemService.fazAcao(personagem, acao, valor);
+    }
+
+    @GetMapping("/açao/id:{id}/{acao}/dado:{dado}")
+    public String acao(@PathVariable Long id, @PathVariable String acao, @PathVariable String dado) {
+        int valor = rolarDado(dado);
+        Personagem personagem = getPersonagem(id);
+        return personagemService.fazAcao(personagem, acao, (long) valor);
     }
 
     @GetMapping("/açao/nome:{nome}/{acao}/{valor}")
     public String acao(@PathVariable String nome, @PathVariable String acao, @PathVariable Long valor) {
         Personagem personagem = getPersonagem(nome);
         return personagemService.fazAcao(personagem, acao, valor);
+    }
+
+    @GetMapping("/açao/nome:{nome}/{acao}/dado:{dado}")
+    public String acao(@PathVariable String nome, @PathVariable String acao, @PathVariable String dado) {
+        int valor = rolarDado(dado);
+        Personagem personagem = getPersonagem(nome);
+        return personagemService.fazAcao(personagem, acao, (long) valor);
     }
 
     @GetMapping("/listar")
